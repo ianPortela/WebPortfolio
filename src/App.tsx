bd30f4943f944de2ca46bd06a17261f0ac85d247
Ian Portela Miranda <ian080406@gmail.com>
2026-06-17 20:05:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer"
+import Content from "./components/Content"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App

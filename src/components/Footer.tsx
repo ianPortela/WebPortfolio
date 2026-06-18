8793b658b310e464b1ebcee0284b0b73cc7192f9
Ian Portela Miranda <ian080406@gmail.com>
2026-06-18 01:11:38 -0300

    feat: creating the footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
+import ContactButton from "./ContentComponents/ContactButton"
+
 function Footer() {
 
     return (
-        <>
-        </>
+        <footer>
+            <ContactButton url="mailto:ian080406@gmail.com" text="Mi mail"/>
+            <ContactButton url="https://github.com/ianPortela" img="src/assets/icons/ic_github.svg"/>
+            <ContactButton url="https://www.linkedin.com/in/ian-portela-miranda-623a64340/" img="src/assets/icons/ic_in.svg"/>
+        </footer>
     )
 }
 

c484660e313d8375cd3a0f7d505eab905d587409
Ian Portela Miranda <ian080406@gmail.com>
2026-07-20 12:19:42 -0300

    feat: centers the icons and text and determines how much space each will occupy

--- src/components/ContentComponents/ContactButton.tsx
+++ src/components/ContentComponents/ContactButton.tsx
@@ -1,13 +1,13 @@
 function ContactButton({url, text, img} : {url:string, text?:string, img?:string}) {
     if(text === undefined){
         return (
-            <a className="flex bg-amber-500 px-6 py-3 rounded-lg items-center" href={url} target="blank">
+            <a className="flex flex-[1] bg-amber-500 px-6 py-3 rounded-lg items-center justify-center" href={url} target="blank">
                 <img className="inline w-8 h-8 invert brightness-0" src={img}/>
             </a>
         )
     } else {
         return (
-            <a className="flex bg-amber-500 px-6 py-3 text-white underline font-bold rounded-lg items-center" href={url} target="blank">
+            <a className="flex flex-[3] bg-amber-500 px-6 py-3 text-white underline font-bold rounded-lg items-center justify-center" href={url} target="blank">
                 {text}
             </a>
         )

e1b64f7c717b761acbb1f918a534767174325ffe
Ian Portela Miranda <ian080406@gmail.com>
2026-07-20 17:37:31 -0300

    feat: add navigation to services section

--- src/components/HeaderComponents/NavBar.tsx
+++ src/components/HeaderComponents/NavBar.tsx
@@ -3,9 +3,10 @@ import LinkToSection from "./LinkToSection"
 function NavBar() {
     return (
         <nav className="flex w-2/3 p-3">
-            <ul className="flex w-full items-center justify-center gap-20">
+            <ul className="flex w-full items-center justify-end gap-10">
                 <li><LinkToSection idSection="profile" text="Perfil"/></li>
                 <li><LinkToSection idSection="projects" text="Proyectos"/></li>
+                <li><LinkToSection idSection="services" text="Servicios"></LinkToSection></li>
                 <li><LinkToSection idSection="knowledge" text="Conocimientos"/></li>
                 <li><LinkToSection idSection="aboutMe" text="Contactame"/></li>
             </ul>

5ef8870fe88d275784c9eea0d76ba2b0c068426b
Ian Portela Miranda <ian080406@gmail.com>
2026-07-20 12:20:40 -0300

    feat: add style to the project card

--- src/components/ContentComponents/ProjectCard.tsx
+++ src/components/ContentComponents/ProjectCard.tsx
@@ -1,18 +1,32 @@
 import Tag, { type TechIcon } from "./Tag";
 
 
-function ProjectCard ({title, imageBackground, technologies} : {title:string, imageBackground?:string, technologies:ReadonlyArray<TechIcon>}) {
+function ProjectCard({ title, imageBackground, technologies }: { title: string, imageBackground?: string, technologies: ReadonlyArray<TechIcon> }) {
     return (
-        <a style={{
-            backgroundImage: imageBackground ? `url(${imageBackground})` : undefined,
-            backgroundSize: "cover",
-            backgroundPosition: "center"
-        }}>
-            <h3>{title}</h3>
-            {technologies.map(tech => (
-                <Tag key={tech} type={tech}/>
-            ))
-            }
+        <a className="group relative h-72 w-full overflow-hidden rounded-3xl border border-neutral-700 bg-neutral-900 transition-all duration-300 hover:-translate-y-1 hover:border-amber-500 hover:shadow-xl p-2">
+
+            <img
+                src={imageBackground}
+                alt={title}
+                className="absolute inset-0 h-full w-full object-cover transition-transform duration-500 group-hover:scale-105"
+            />
+
+            <div className="absolute inset-0 bg-gradient-to-t from-black via-black/50 to-transparent" />
+
+            <div className="absolute bottom-0 flex w-full flex-col gap-4 p-6">
+
+                <h3 className="text-2xl font-bold text-white">
+                    {title}
+                </h3>
+
+                <div className="flex flex-wrap gap-2">
+                    {technologies.map((tech) => (
+                        <Tag key={tech} type={tech} />
+                    ))}
+                </div>
+
+            </div>
+
         </a>
     )
 }

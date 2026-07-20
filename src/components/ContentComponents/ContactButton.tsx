5a6613ed82165ebbee6609c199e0771074f519f9
Ian Portela Miranda <ian080406@gmail.com>
2026-07-19 22:47:03 -0300

    feat: center the content of contact button

--- src/components/ContentComponents/ContactButton.tsx
+++ src/components/ContentComponents/ContactButton.tsx
@@ -1,13 +1,13 @@
 function ContactButton({url, text, img} : {url:string, text?:string, img?:string}) {
     if(text === undefined){
         return (
-            <a className="bg-amber-500 px-6 py-3 rounded-lg" href={url} target="blank">
+            <a className="flex bg-amber-500 px-6 py-3 rounded-lg items-center" href={url} target="blank">
                 <img className="inline w-8 h-8 invert brightness-0" src={img}/>
             </a>
         )
     } else {
         return (
-            <a className="bg-amber-500 px-6 py-3 text-white underline font-bold rounded-lg" href={url} target="blank">
+            <a className="flex bg-amber-500 px-6 py-3 text-white underline font-bold rounded-lg items-center" href={url} target="blank">
                 {text}
             </a>
         )

57f07e025f848fbb1cfefb70ecac63ac35ec19d6
Ian Portela Miranda <ian080406@gmail.com>
2026-07-23 15:16:07 -0300

    feat: change border width

--- src/components/ContentComponents/ContactButton.tsx
+++ src/components/ContentComponents/ContactButton.tsx
@@ -6,7 +6,7 @@ function ContactButton({url, text, img, color} : {url:string, text?:string, img?
     if(text === undefined){
         return (
             <a
-                className={`group flex flex-[1] ${color} items-center justify-center rounded-lg border border-neutral-600 px-6 py-3 transition-all duration-300 hover:-translate-y-1 hover:border-amber-500 hover:shadow-lg hover:brightness-110`}
+                className={`group flex flex-[1] ${color} items-center justify-center rounded-lg border-3 border-neutral-600 px-6 py-3 transition-all duration-300 hover:-translate-y-1 hover:border-amber-500 hover:shadow-lg hover:brightness-110`}
                 href={url}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -21,7 +21,7 @@ function ContactButton({url, text, img, color} : {url:string, text?:string, img?
     } else {
         return (
             <a
-                className={`group flex flex-[3] ${color} items-center justify-center rounded-lg border border-neutral-600 px-6 py-3 font-bold text-white transition-all duration-300 hover:-translate-y-1 hover:border-amber-500 hover:shadow-lg hover:brightness-110`}
+                className={`group flex flex-[3] ${color} items-center justify-center rounded-lg border-3 border-neutral-600 px-6 py-3 font-bold text-white transition-all duration-300 hover:-translate-y-1 hover:border-amber-500 hover:shadow-lg hover:brightness-110`}
                 href={url}
                 target="_blank"
                 rel="noopener noreferrer"

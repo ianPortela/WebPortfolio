670832ea3493776f36ca6ba0034fcc0030470247
Ian Portela Miranda <ian080406@gmail.com>
2026-07-24 14:01:03 -0300

    refactor: delete TechIcon import

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom"
-import { type TechIcon } from "../components/ContentComponents/Tag"
 import Tag from "../components/ContentComponents/Tag"
 import Subtitle from "../components/Subtitle"
 import { projects } from "../assets/ProjectsData"
